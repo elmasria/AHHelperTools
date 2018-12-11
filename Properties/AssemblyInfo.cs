9a6c22fdf7b60def3a4d2150824e7f71390eab1d
Ahmad EL Masri <ahmadengineer@hotmail.com>
2018-12-11 15:31:51 +0400

    chore: update version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Helper Tools")]
-[assembly: AssemblyDescription("Could be used to facilitate logs, and other functionality")]
+[assembly: AssemblyDescription("Facilitate logs, and other functionalities when building a .Net application")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("AHHelperTools")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
