11bf62ddb635c0984a21cae2d4020fb73502cd86
Ahmad EL Masri <ahmadengineer@hotmail.com>
2018-12-18 09:03:12 +0400

    feat: add - log exception details method

--- AHlogs.cs
+++ AHlogs.cs
@@ -3,7 +3,7 @@ using System.IO;
 
 namespace AHHelperTools
 {
-    public class AHlogs
+    public class AHLogs
     {
         static string LOGPATH = System.AppDomain.CurrentDomain.BaseDirectory.ToString() + @"ah-logs.txt";
 
@@ -12,7 +12,7 @@ namespace AHHelperTools
         /// by the tool. and with the current time.
         /// </summary>
         /// <param name="message"></param>
-        public static void log(string message)
+        public static void Log(string message)
         {
             using (StreamWriter file = new StreamWriter(LOGPATH, true))
             {
@@ -28,7 +28,7 @@ namespace AHHelperTools
         /// <param name="message"></param>
         /// <param name="fileName"></param>
         /// <param name="withTime"></param>
-        public static void log(string message, string fileName = "ah-logs", Boolean withTime = true)
+        public static void Log(string message, string fileName = "ah-logs", Boolean withTime = true)
         {
             if (fileName != "ah-logs")
             {
@@ -42,10 +42,47 @@ namespace AHHelperTools
                     file.WriteLine(currentTime + " - " + message);
                 }
                 else
-                {
-                    DateTime currentTime = DateTime.Now;
+                { 
                     file.WriteLine(message);
                 }
+            }
+        }
+
+        /// <summary>
+        /// logging Exception details
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void Log(Exception ex)
+        {
+            using (StreamWriter file = new StreamWriter(LOGPATH, true))
+            {
+                DateTime currentTime = DateTime.Now;
+                Guid expIDTrackerr = Guid.NewGuid();
+
+                file.WriteLine(string.Format("{0} - Exception Details: {1} - {2}", currentTime, ex.Message, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Inner Exception: {1} - {2}", currentTime, ex.InnerException, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Source: {1} - {2}", currentTime, ex.Source, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Type: {1} - {2}", currentTime, ex.GetType(), expIDTrackerr));
+            }
+        }
+
+        /// <summary>
+        /// logging custom message with Exception details
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="ex"></param>
+        public static void Log(string message, Exception ex)
+        {
+            using (StreamWriter file = new StreamWriter(LOGPATH, true))
+            {
+                DateTime currentTime = DateTime.Now;
+                Guid expIDTrackerr = Guid.NewGuid();
+                
+                file.WriteLine(string.Format("{0} - Message: {1} - {2}", currentTime, message, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Exception Details: {1} - {2}", currentTime, ex.Message, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Inner Exception: {1} - {2}", currentTime, ex.InnerException, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Source: {1} - {2}", currentTime, ex.Source, expIDTrackerr));
+                file.WriteLine(string.Format("{0} - Type: {1} - {2}", currentTime, ex.GetType(), expIDTrackerr));
             }
         }
     }
